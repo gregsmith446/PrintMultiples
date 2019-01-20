1049e682d9ccc2216f13a1557726e972d87211ec
Greg Smith <gregsmith446@gmail.com>
2019-01-19 19:25:04 -0500

    completed challenge

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintMultiplesWithLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("number plz:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int result = n * i;
+
+                Console.Write(n);
+                Console.Write(" x ");
+                Console.Write(i);
+                Console.Write(" = ");
+                Console.WriteLine(result);
+
+                // Console.WriteLine("{n} x {i} = {result}");
+            }
+        }
+    }
+}
